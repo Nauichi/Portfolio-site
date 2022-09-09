5206f3eb1f3aac487eb5cf72740e4cd492922d3a
nauichi <snarishoyu3259@gmil.com>
2022-09-09 11:17:56 +0900

    Portfolio作成中

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -517,13 +517,6 @@ import FadeIn from './FadeIn.vue';
 import SlideIn from './SlideIn.vue';
 export default {
     name: "NuxtTutorial",
-    head: {
-      meta: [
-        { hid: 'og:title', property: 'og:title', content: 'My Portfolio site' },
-        { hid: 'og:image', property: 'og:image', content: 'MyPortfolio.png' },
-        { hid: 'og:description', property: 'og:description', content: 'ワイのポートフォリオ' },
-      ],
-    },
     data() {
         return {
             Birthdate: {
--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,5 @@
 <script>
 export default {
   name: 'IndexPage',
-  head: {
-      meta: [
-        { hid: 'og:title', property: 'og:title', content: 'My Portfolio site' },
-        { hid: 'og:image', property: 'og:image', content: 'MyPortfolio.png' },
-        { hid: 'og:description', property: 'og:description', content: 'ワイのポートフォリオ' },
-      ],
-    },
 }
 </script>
