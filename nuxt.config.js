63499c52a91f866b80f1a8dcf40416f8203b2ed1
nauichi <snarishoyu3259@gmil.com>
2022-09-09 11:49:52 +0900

    Portfolio作成中

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,14 +14,15 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '僕のポートフォリオ、見てってください' },
+      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'My_Portfolio-site' },
-      { hid: 'og:url', property: 'og:url', content: 'MyPortfolio' },
+      { hid: 'og:url', property: 'og:url', content: 'My_Portfolio' },
       { hid: 'og:description', property: 'og:description', content: '僕のポートフォリオ、見てってください' },
-      { hid: 'og:image', property: 'og:image', content: '/mogura.png' },
+      { hid: 'og:image', property: 'og:image', content: '/hoge.jpg' },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/main.ico' }],
