f25b9ad3a880a9e1857b209f502e8aa850a82618
nauichi <snarishoyu3259@gmil.com>
2022-09-13 11:11:40 +0900

    Portfolio作成中

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,16 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/main.ico' },
-      { rel: 'apple-touch-startup-image',  href: '/icon_pwa.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', href: '/iphone5_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: '/iphone6_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)', href: '/iphoneplus_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', href: '/iphonex_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', href: '/iphonexr_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', href: '/iphonexsmax_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)', href: '/ipad_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', href: '/ipadpro1_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', href: '/ipadpro3_splash.png' },
+      { rel: 'apple-touch-startup-image', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', href: '/ipadpro2_splash.png' }
     ],
   },
 
